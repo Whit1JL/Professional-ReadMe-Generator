5fafd5bd07cf96dac5b492ba6f06cb08cd66d0f9
Jessica White <jessica2714white@gmail.com>
2021-08-05 20:43:54 -0500

    Added more to variable generate

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// 
+// Generate a professional ReadMe file
+
 var inquirer = require('inquirer');
 const fs = require('fs');
 var template = '';
@@ -23,18 +24,28 @@ var generate = function (data) {
     ## Description
     ${data.describe}
 
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
     ## License
     ${data.license}
+
+    ## Contribute
+    ${data.contribute}
+
+    ## Test
+    ${data.test}
+    
+    ## Questions
+    ${data.questions}
     `
     console.log(template);
 
     writeFile();
 }
-
-
-
-// function results()
-
 
 inquirer
     .prompt([
@@ -44,18 +55,48 @@ inquirer
             name: "title",
             message: "What's the name of your repository?",
         },
-        // include more questions for other parameters
         {
             type: "input",
             name: "describe",
-            message: "Please provide a description of your repository.",
+            message: "Provide a description of your repository.",
+        },
+        // include table of contents that links important documents
+        // {
+        //     type: "input",
+        //     name: "tableContents",
+        //     message: "",
+        // },
+        {
+            type: "input",
+            name: "installation",
+            message: "Provide specific steps needed to install this repository.",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How do you use this application?",
         },
         {
             type: "list",
             name: "license",
             message: "What's licenses does your repository have?",
             choices: ["MIT", "Apache license 2.0", "ISC", "Other"],
-        }
+        },
+        {
+            type: "input",
+            name: "contribute",
+            message: "What contributions did you make to this repository?",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "",
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "",
+        },
     ])
     .then((answers) => {
         // Use user feedback for... whatever!!
