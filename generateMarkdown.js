bd7e2176cdc6ba2d82db227484b20c3e757234c0
Jessica White <jessica2714white@gmail.com>
2021-08-17 17:16:32 -0500

    created render lincense section function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -34,6 +34,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+    if (license === "none") {
+        return "";
+    } else {
+        return `License is ${renderLicenseLink(license)} standard license.`;
+    }
 }
 
 // Create a function to generate markdown for README
