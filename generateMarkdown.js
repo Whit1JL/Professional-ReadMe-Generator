e8245ff5c111f81a7676ed40dee1e14c8545040f
Jessica White <jessica2714white@gmail.com>
2021-08-17 16:55:33 -0500

    added finishing touches to function that renders license badge

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,29 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { 
-//     if {
-
-//     }
-//     else {
-//         return null;
-//     }
-// }
+function renderLicenseBadge(license) {
+    if (`${this.license}` === "MIT") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);`
+    } if (`${this.license}` === "Apache") {
+        return `[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0);`
+    } if (`${this.license}` === "ISC") {
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC);`
+    } if (`${this.license}` === "Zlib") {
+        return `[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib);`
+    } else if (license === "none") {
+        return "no license selected!"
+    }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return template = `# Title
-   ${data.title}
+    return template = `
+    
+# Title
+    ${data.title}
 
-  ## Table of Contents 
 
+## Table of Contents 
   * [Installation](#Installation)
   * [Usage](#Usage)
   * [Contributing](#Contributing)
@@ -31,34 +40,36 @@ function generateMarkdown(data) {
   * [Questions](#Questions)
 
 
-  ${renderLicenseBadge(data.license)}
+${renderLicenseBadge(data.license)}
 
-    ## Description
+## Description
     ${data.describe}
 
-    ## Installation
+## Installation
     ${data.installation}
 
-    ## Usage
+## Usage
     ${data.usage}
 
-    ## License
+## License
     ${data.license}
 
-    ## Contribute
+## Contribute
     ${data.contribute}
 
-    ## Test
+## Test
     ${data.test}
 
-    ## Questions
+## Questions
     ${data.questions}
+
+## GitHub
+    ${data.github}
+
+## Email
+    ${data.email}
     `
-    // console.log(template);
-
-    // writeFile();
-
 };
 
 module.exports = generateMarkdown;
-}
+
